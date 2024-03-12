2c5da6d91a168fbec60fd01984e68546000dd551
TOP-FDesmet <florian.desmet@topchretien.com>
2024-03-12 15:00:35 +0100

    exercice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HelloComponent} from "./components/hello/hello.component";
+import {UserService} from "./services/user/user.service";
+import {UserComponent} from "./components/user/user.component";
+import {CounterComponentComponent} from "./components/counter-component/counter-component.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, HelloComponent, UserComponent, CounterComponentComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'firstApp';
+    title = 'firstApp';
+    users: string[];
+
+    constructor(private userService: UserService) {
+        this.users = this.userService.getUsers();
+    }
 }
